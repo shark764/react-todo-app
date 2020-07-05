d98798fe6bc7f55b5fe891f3f25a26a304c8fb52
Farid Hernandez <farid.hdz.64@gmail.com>
2020-07-05 14:54:24 -0600

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    semi: ['error', 'always'],
+    'arrow-parens': ['error', 'as-needed'],
+    'react/prop-types': ['error', { ignore: ['navigation'], skipUndeclared: true }],
+    'react/jsx-props-no-spreading': ['error', { custom: 'ignore' }],
+    'global-require': 0,
+    'no-console': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 150,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+import Form from './components/Form';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>This is an incredibly complex react app!</p>
+        <br />
+        <Form />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
